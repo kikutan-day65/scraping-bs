2ee64cd01844dd115ca0abb7a067bfb0236b06be
Haruto Mori <baystarsman@outlook.jp>
2023-03-21 20:18:46 +0100

    save titles and authors in the csv file

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import sys
+import csv
 import requests
 from bs4 import BeautifulSoup
 
@@ -18,5 +19,14 @@ authors = main_content.find_all('span', class_='entry-author-name')
 title_list = [title.text for title in titles[:10]]
 author_list = [author.text for author in authors[:10]]
 
-for article in zip(title_list, author_list):
-    print(article)
+# Open a new CSV file in write mode
+with open('articles.csv', 'w', newline='', encoding='utf-8') as article:
+
+    # Create a CSV writer object
+    csvwriter = csv.writer(article)
+
+    # Write the header row
+    csvwriter.writerow(['Title', 'Author'])
+
+    for title, author in zip(title_list, author_list):
+        csvwriter.writerow([title, author])
