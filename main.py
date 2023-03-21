b0d610e17d6be77cf78134250ae71e795afa2dbc
Haruto Mori <baystarsman@outlook.jp>
2023-03-21 19:22:30 +0100

    get the first 10 articles and its authors

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from bs4 import BeautifulSoup
 # Set default encoding to utf-8
 sys.stdout.reconfigure(encoding='utf-8')
 
-url = 'https://www.mlbtraderumors.com/cleveland-guardians/'
+url = 'https://www.mlbtraderumors.com/cleveland-guardians?show=all'
 page = requests.get(url)
 
 soup = BeautifulSoup(page.content, 'lxml')
@@ -15,5 +15,8 @@ main_content = soup.find('main')
 titles = main_content.find_all('h2', class_='entry-title')
 authors = main_content.find_all('span', class_='entry-author-name')
 
-title_list = [title.text for title in titles]
-author_list = [author.text for author in authors]
+title_list = [title.text for title in titles[:10]]
+author_list = [author.text for author in authors[:10]]
+
+for article in zip(title_list, author_list):
+    print(article)
